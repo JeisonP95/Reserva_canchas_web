69d2630c3a14fada320e5a41039295f55ed1bf63
Jhoan Castro Santacruz <cjhoan735@gmail.com>
2025-04-08 17:09:27 -0500

    ajuste modal y home

--- src/layouts/Modal/Modal.tsx
+++ src/layouts/Modal/Modal.tsx
@@ -1,36 +1,34 @@
-import React from 'react';
-import CustomButton from '../../components/Buttons/CustomButton';
-import './Modal.css';
+import React from "react"
+import CustomButton from "../../components/Buttons/CustomButton"
+import "./Modal.css"
 
 interface ModalProps {
-  titleModal: string;
-  children: React.ReactNode;
-  onConfirm: () => void;
-  onCancel: () => void;
+  titleModal: string
+  children: React.ReactNode
+  onConfirm: () => void
+  onCancel: () => void
 }
 
-const Modal: React.FC<ModalProps> = ({
-  titleModal,
-  children,
-  onConfirm,
-  onCancel
-}) => {
+const Modal: React.FC<ModalProps> = ({ titleModal, children, onConfirm, onCancel }) => {
   return (
     <div className="modal-overlay-component">
       <div className="modal-container-component">
         <div className="modal-header-component">
-          {titleModal}
+          <h3 className="modal-title">{titleModal}</h3>
+          <button className="modal-close-button" onClick={onCancel}>
+            ×
+          </button>
         </div>
         <div className="modal-content-component">
-         {children}
+          {children}
           <div className="modal-footer-component">
-            <CustomButton variant='secondary' text='Cancelar' onClick={onCancel}/>            
-            <CustomButton variant='primary' text='Aceptar' onClick={onConfirm}/>             
+            <CustomButton variant="secondary" text="Cancelar" onClick={onCancel} />
+            <CustomButton variant="primary" text="Aceptar" onClick={onConfirm} />
           </div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Modal;
+export default Modal
